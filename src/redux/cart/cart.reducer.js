fed534e01f806bb05c8283b8bb9fee845848e239
Trung Pham <trungpham2012@gmail.com>
2019-07-10 18:31:42 -0500

    added addItem and removeItem to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
 	hidden: true,
@@ -11,7 +11,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.TOGGLE_CART_HIDDEN:
 			return {...state, hidden: !state.hidden};
 		case CartActionTypes.ADD_ITEM:
-			return {...state, cartItems: addItemToCart(state.cartItems, action.payload)};	
+			return {...state, cartItems: addItemToCart(state.cartItems, action.payload)};
+		case CartActionTypes.REMOVE_ITEM:
+			return {...state, cartItems: removeItemFromCart(state.cartItems, action.payload)};
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state, cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+			}
 		default:
 			return state;
 	}
