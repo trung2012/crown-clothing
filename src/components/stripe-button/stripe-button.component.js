05450ba459bca1ad9d6c296c5dc479cbc8e5441a
Trung Pham <trungpham2012@gmail.com>
2019-07-11 16:44:10 -0500

    add stripe payment and updated favicon

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_QanDgFI6H3tZ5GNjkqBois7i00izJwzf5n';
+
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+	};
+
+	return (
+		<StripeCheckout
+			label='Pay Now'
+			name='Crown Clothing Ltd.'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/CUz.svg'
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+}
+
+export default StripeCheckoutButton;
