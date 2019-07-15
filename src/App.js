5da291d6a48c3eda7a7cdbca732581887598651d
Trung Pham <trungpham2012@gmail.com>
2019-07-15 12:28:42 -0500

    moved sign in to redux sagas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom';
-import { auth ,createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
 import { createStructuredSelector } from 'reselect';
 
 import ContactPage from './pages/contact/contact.component';
@@ -20,21 +18,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          });          
-        })
-      }
-      setCurrentUser(userAuth);
-    })
+    //     userRef.onSnapshot(snapShot => {
+    //       setCurrentUser({
+    //           id: snapShot.id,
+    //           ...snapShot.data()
+    //       });          
+    //     })
+    //   }
+    //   setCurrentUser(userAuth);
+    // })
   }
 
   componentWillUnmount() {
@@ -61,8 +58,5 @@ const mapStateToProps = state => createStructuredSelector({
   currentUser: selectCurrentUser
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
-import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -16,16 +18,10 @@ class SignIn extends React.Component {
 	handleSubmit = async event => {
 		event.preventDefault();
 
+		const { emailSignInStart } = this.props;
 		const { email, password } = this.state;
 
-		try {
-			await auth.signInWithEmailAndPassword(email, password);
-			this.setState({email: '', password: ''});
-		}
-
-		catch(error) {
-			console.log(error);
-		}
+		emailSignInStart(email, password);
 
 	}
 
@@ -36,6 +32,8 @@ class SignIn extends React.Component {
 	}
 
 	render() {
+		const { googleSignInStart } = this.props;
+
 		return (
 			<div className='sign-in'>
 				<h2 className='title'>I already have an account</h2>
@@ -57,7 +55,12 @@ class SignIn extends React.Component {
 					label='password'/>
 					<div className='buttons'>
 						<CustomButton type='submit'>Sign In</CustomButton>
-						<CustomButton onClick ={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+						<CustomButton 
+							type='button'
+							onClick ={googleSignInStart} 
+							isGoogleSignIn>
+							Sign In With Google
+						</CustomButton>
 					</div>
 				</form>
 			</div>
@@ -65,4 +68,9 @@ class SignIn extends React.Component {
 	}
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+	googleSignInStart: () => dispatch (googleSignInStart()),
+	emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password})) 
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
