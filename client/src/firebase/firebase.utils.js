de91f6c25be4a052641f310144c1392e729ad9b9
Trung Pham <trungpham2012@gmail.com>
2019-07-17 15:13:24 -0500

    finalized server and some small tweaks

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
  
 const config = {
-    apiKey: "AIzaSyC3OUChMZ_9rfu3ebVPC9L0P8nylY90NNc",
-    authDomain: "crown-db-ed113.firebaseapp.com",
-    databaseURL: "https://crown-db-ed113.firebaseio.com",
-    projectId: "crown-db-ed113",
-    storageBucket: "",
-    messagingSenderId: "342327869809",
-    appId: "1:342327869809:web:d69f60ec9a4159c0"
-  };
+  apiKey: "AIzaSyC3OUChMZ_9rfu3ebVPC9L0P8nylY90NNc",
+  authDomain: "crown-db-ed113.firebaseapp.com",
+  databaseURL: "https://crown-db-ed113.firebaseio.com",
+  projectId: "crown-db-ed113",
+  storageBucket: "",
+  messagingSenderId: "342327869809",
+  appId: "1:342327869809:web:d69f60ec9a4159c0"
+};
  
 firebase.initializeApp(config);
 
